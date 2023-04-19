7c478e50f2d67aa50148f078c61b73f2db396c87
Sadman Ahmed Shanto <shanto@usc.edu>
2023-04-18 22:31:12 -0700

    contour extraction done

---
+++ README.md
@@ -0,0 +1,52 @@
+# Image Analysis Program for Scanning Electron Microscopy (SEM) Pictures of Nano-bridge Resonator Devices (NBR):
+
+
+This Python script processes SEM images, detects contours, and saves the contour images in a specified output directory. The script also crops the bottom 10% of the image before processing so that SEM information is not included in the contour analysis.
+
+## Requirements
+
+- Python 3.x
+- OpenCV
+- scikit-image
+- imutils
+
+You can install the required libraries using the following command:
+
+```bash
+pip install opencv-python scikit-image imutils
+```
+
+## Usage
+
+1. Save the script as `get_device_contour.py`
+2. Prepare a directory with SEM images you want to process.
+3. Open a terminal and navigate to the directory where the script is saved.
+
+Run the following command:
+
+```
+python get_device_contour.py --input /path/to/your/input/directory
+```
+
+This command processes all the images in the specified input directory and saves the contour images in the "results" directory within the script's location.
+
+You can change the low contrast threshold by adding the -t or --thresh argument, followed by a float value. For example:
+
+```
+python get_device_contour.py --input /path/to/your/input/directory --thresh 0.05
+```
+
+The default value for the threshold is 0.05.
+
+
+---
+
+## To Do:
+
+- [ ] Measure the sizes of various features based on one measurement/the scale
+
+- [ ] Generate analysis plots for dose tests of various features
+
+- [ ] ML integration so that it can be trained based on inference codes for image classification (good bridge/dosimetry classification) in the future to speed up analysis task
+
+
---
+++ get_device_contour.py
@@ -0,0 +1,47 @@
+import os
+import cv2
+import argparse
+import numpy as np
+import imutils
+from imutils.paths import list_images
+from skimage.exposure import is_low_contrast
+
+def process_directory(input_dir, results_dir):
+    imagePaths = sorted(list(list_images(input_dir)))
+    for (i, imagePath) in enumerate(imagePaths):
+        print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
+        image = cv2.imread(imagePath)
+        image = imutils.resize(image, width=450)
+        
+        # Crop the bottom 10% of the image
+        height, _, _ = image.shape
+        cropped_img = image[:int(height * 0.9), :, :]
+        
+        gray = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+        edged = cv2.Canny(blurred, 30, 150)
+        
+        cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+
+        contour_img = np.zeros_like(cropped_img)
+        for c in cnts:
+            cv2.drawContours(contour_img, [c], -1, (255, 255, 255), 2)
+
+        output_filename = os.path.join(results_dir, os.path.basename(imagePath))
+        cv2.imwrite(output_filename, contour_img)
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--input", required=True, help="path to input directory of images")
+    ap.add_argument("-t", "--thresh", type=float, default=0.05, help="threshold for low contrast")
+    args = vars(ap.parse_args())
+    
+    input_dir = args["input"]
+    results_dir = "results"
+
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+
+    process_directory(input_dir, results_dir)
+
